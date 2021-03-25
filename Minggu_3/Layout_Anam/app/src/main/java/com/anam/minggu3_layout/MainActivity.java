f45c258171c4666fe933f2bc7395fcb02c8258ff
mhaerulanam <mhaerulanam603@gmail.com>
2021-03-25 21:17:25 +0700

    Minggu 3 (Material Design & Scroll View)

--- Minggu_3/Layout_Anam/app/src/main/java/com/anam/minggu3_layout/MainActivity.java
+++ Minggu_3/Layout_Anam/app/src/main/java/com/anam/minggu3_layout/MainActivity.java
@@ -2,25 +2,43 @@ package com.anam.minggu3_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import android.text.Selection;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Pada, Subject, Pesan;
-    Button Submit;
+//    EditText Pada, Subject, Pesan;
+//    Button Submit;
+
+//    TextView dateTimeDisplay;
+//    Calendar calendar;
+//    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
         // Perintah untuk menampilkan activity linear layout
 //      setContentView(R.layout.activity_linear);
 
         //Perintah untuk menampilkan activity Relative layout
-//      setContentView(R.layout.activity_relative);
+        setContentView(R.layout.activity_relative);
 
         //Perintah untuk menampilkan activity constraint layout
 //      setContentView(R.layout.activity_constraint);
@@ -36,5 +54,17 @@ public class MainActivity extends AppCompatActivity {
 //        Subject = findViewById(R.id.subject);
 //        Pesan = findViewById(R.id.pesan);
 //        Submit = findViewById(R.id.submit);
+        // Get reference of widgets from XML layout
+
+//        dateTimeDisplay = findViewById(R.id.text_date_display);
+//
+//        calendar = Calendar.getInstance();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EE, MMM d, yyyy");
+//        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("h:mm a");
+//        date = simpleDateFormat.format(calendar.getTime());
+//        String date2 = simpleDateFormat2.format(calendar.getTime());
+//        dateTimeDisplay.setText(date);
+//        dateTimeDisplay.setText(date2);
     }
+
 }
