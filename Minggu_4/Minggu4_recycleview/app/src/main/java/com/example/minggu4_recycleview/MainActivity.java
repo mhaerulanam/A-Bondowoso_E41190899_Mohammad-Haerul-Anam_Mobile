6fc58e7cce12cd162be78c189957e0cebb69e039
mhaerulanam <mhaerulanam603@gmail.com>
2021-03-30 15:19:46 +0700

    Minggu4 (RecycleView)

--- Minggu_4/Minggu4_recycleview/app/src/main/java/com/example/minggu4_recycleview/MainActivity.java
+++ Minggu_4/Minggu4_recycleview/app/src/main/java/com/example/minggu4_recycleview/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.minggu4_recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addData();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Mohammad Haerul Anam, "235102763", "123456789"));
+        mahasiswaArrayList.add(new Mahasiswa("Mohammad , "8908977", "123456789"));
+        mahasiswaArrayList.add(new Mahasiswa("Haerul , "1283454", "123456789"));
+        mahasiswaArrayList.add(new Mahasiswa("Anam, "49336494", "123456789"));
+
     }
 }
