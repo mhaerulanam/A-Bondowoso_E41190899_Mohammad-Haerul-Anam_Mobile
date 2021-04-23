fcf0c49e77159d0798b5067acc875dcecea23ab4
mhaerulanam <mhaerulanam603@gmail.com>
2021-04-23 23:42:22 +0700

    Minggu7_(ManjemenFile)

---
+++ Minggu_7/Minggu7_ManajemenFile/app/src/main/java/com/example/minggu7_manjementfile_sqlite/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.minggu7_manjementfile_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    private int STORAGE_PERMISSION_CODE = 22;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText2);
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(MainActivity.this, MainTwoActivity.class);
+        startActivity(intent);
+    }
+
+    public void savePublic(View view) {
+        //Permission to access external storage
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        String info = editText.getText().toString();
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);// Nama Folder
+        File myFile = new File(folder, "myData01.txt");// Nama file
+        writeData(myFile, info);
+        editText.setText("");
+
+    }
+
+    public void savePrivate(View view) {
+        String info = editText.getText().toString();
+        File folder = getExternalFilesDir("Anam");// Nama Folder
+        File myFile = new File(folder, "myData02.txt");// Nama File
+        writeData(myFile, info);
+        editText.setText("");
+    }
+
+    private void writeData(File myFile, String data) {
+        FileOutputStream fileOutputStream = null;
+        try {
+            System.out.println("TES");
+            fileOutputStream = new FileOutputStream(myFile);
+            fileOutputStream.write(data.getBytes());
+            Toast.makeText(this, "Done" + myFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ Minggu_7/Minggu7_ManajemenFile/app/src/main/java/com/example/minggu7_manjementfile_sqlite/MainTwoActivity.java
@@ -0,0 +1,82 @@
+package com.example.minggu7_manjementfile_sqlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainTwoActivity extends AppCompatActivity {
+
+    TextView TextTampil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_two);
+        TextTampil = (TextView) findViewById(R.id.getText);
+    }
+
+
+    public void getPublic(View view) {
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS); //Nama Folder
+        File myFile = new File(folder, "myData01.txt"); // Nama File
+        String text = getdata(myFile);
+        String tampil = text;
+        if (text != null) {
+            TextTampil.setText(text);
+        } else {
+            TextTampil.setText("No Data");
+        }
+        Toast.makeText(this,  tampil, Toast.LENGTH_SHORT).show();
+    }
+
+    public void getPrivate(View view) {
+        File folder = getExternalFilesDir("Anam"); // Nama Folder
+        File myFile = new File(folder, "myData02.txt"); // Nama File
+        String text = getdata(myFile);
+        String tampil = text;
+        if (text != null) {
+            TextTampil.setText(text);
+        } else {
+            TextTampil.setText("No Data");
+        }
+        Toast.makeText(this,  tampil, Toast.LENGTH_SHORT).show();
+    }
+
+    private String getdata(File myfile) {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(myfile);
+            int i = -1;
+            StringBuffer buffer = new StringBuffer();
+            while ((i = fileInputStream.read()) != -1) {
+                buffer.append((char) i);
+            }
+            return buffer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(MainTwoActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
